39703b9749ee509d1364b0818577fad2a8de4bb0
Cesar Prados <bradomyn@localhost.localdomain>
2012-11-30 12:05:13 +0100

    logging service already complete with filters

---
+++ test_wr_log.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+
+import wr_log
+
+
+wr_log.log.debug("DEBUGGG")
+
+wr_log.log.error("errro")
+wr_log.log.info("info")
+wr_log.log.warning("Warning")
+wr_log.log.critical("Critial")
--- wr_log.py
+++ wr_log.py
@@ -1,21 +1,53 @@
 import logging
 
+# Filter
+class InfoFilter(logging.Filter):
+    def filter(self, rec):
+            return rec.levelno == logging.INFO
+
+class DebugFilter(logging.Filter):
+    def filter(self, rec):
+            return rec.levelno == logging.DEBUG
+
+
 log = logging.getLogger("wrlog")
-log.setLevel(logging.DEBUG)
+##################################
+# setLevel in the logger determines 
+# which severity of messages it will 
+# pass to its handlers 
+##################################
+log.setLevel(logging.DEBUG)     # All the Msg 
+#log.setLevel(logging.INFO)     # Error, Critical, Warning, Info Msg
+#log.setLevel(logging.WARNING)  # Error, Critical and Warning Msg
+#log.setLevel(logging.ERROR)    # Error and Critical Msg
 
 #formatter = logging.Formatter("%(asctime)s %(threadName)-11s %(levelname)-10s %(message)s")
 formatter = logging.Formatter("%(asctime)s %(module)-11s %(levelname)-10s %(message)s")
 
-# Log to file
-filehandler = logging.FileHandler("log", "w")
-filehandler.setLevel(logging.INFO)
-filehandler.setFormatter(formatter)
-log.addHandler(filehandler)
-
-# Log to stdout too
+# Log to stdout
 streamhandler = logging.StreamHandler()
-streamhandler.setLevel(logging.DEBUG)
+##################################
+# setLevel in each handler determines 
+# which messages that handler will send on.
+##################################
+streamhandler.setLevel(logging.DEBUG)     # All the Msg
+#streamhandler.setLevel(logging.INFO)     # Error, Critical, Warning, Info Msg
+#streamhandler.setLevel(logging.WARNING)  # Error, Critical and Warning Msg
+#streamhandler.serLevel(logging.ERROR)    # Error and Critical Msg
+
+
 streamhandler.setFormatter(formatter)
 log.addHandler(streamhandler)
+
+# Addign the filter as you please
+#log.addFilter(DebugFilter())
+#log.addFilter(InfoFilter())
+
+# Log to file
+#filehandler = logging.FileHandler("log", "w")
+#filehandler.setLevel(logging.INFO)
+#filehandler.setFormatter(formatter)
+#log.addHandler(filehandler)
+
 
 
